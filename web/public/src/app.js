f5a1b0b39196e87fb520ac1c2f58780c5e0863bb
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2019-09-05 13:39:46 +0200

    modify editorconfig, gitignore, package.json to lunch webpack as development mode and the app.js with the include of the math function

--- web/public/src/app.js
+++ web/public/src/app.js
@@ -1,5 +1,8 @@
 // import css
 import "./style.scss"
 
+import { add } from './mathFunctions.js';
+
 // Real code to execute
 document.getElementById("btn-alert").addEventListener("click", showAlert);
+console.log(add(2+2));
