a57e2f918ae37d7bf52dfb7cdbf7413ce288f00f
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2019-09-09 16:21:46 +0200

    wip

--- web/public/src/app.js
+++ web/public/src/app.js
@@ -3,11 +3,11 @@ import './scss/style.scss';
 
 import { add } from './js/mathFunctions';
 
-const y = add(2 + 2);
+const y = add(2, 2);
 console.log(y);
 
 // Real code to execute
 document.getElementById('btn-alert').addEventListener('click', () => {
-  const x = add(2 + 2);
+  const x = add(2, 2);
   alert(x);
 });
