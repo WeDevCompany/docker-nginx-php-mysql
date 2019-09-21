63021ad6e30c34df7baa9176c0d15505efaf3cbe
khru <evrtrabajo@gmail.com>
2019-09-22 01:22:46 +0200

    Testing webpackconfig

--- web/public/src/app.js
+++ web/public/src/app.js
@@ -1,13 +1,15 @@
 // import css
 import './scss/style.scss';
 
-import { add } from './js/mathFunctions';
+import {
+    add
+} from './js/mathFunctions';
 
 const y = add(2, 2);
 console.log(y);
 
 // Real code to execute
 document.getElementById('btn-alert').addEventListener('click', () => {
-  const x = add(2, 2);
-  alert(x);
+    const x = add(2, 2);
+    alert(x);
 });
